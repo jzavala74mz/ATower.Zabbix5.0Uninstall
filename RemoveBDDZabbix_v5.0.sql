b02a79f8fa61db223f34812c76995359a7d6ee69
root <root@atower.labrhel8>
2020-12-20 10:10:12 -0800

    Cambios IF EXISTS al query para eliminar las bases de datos y el usuario

--- RemoveBDDZabbix_v5.0.sql
+++ RemoveBDDZabbix_v5.0.sql
@@ -1,7 +1,7 @@
 -- Remover los elementos de la base de datos de Zabbix
 
 -- Eliminar la base de datos
-DROP DATABASE IF EXISTS ZABBIX;
+DROP DATABASE IF EXISTS 'zabbix';
 
 -- Eliminar el usuario de Zabbix
 DROP USER IF EXISTS 'zabbix'@'localhost';
