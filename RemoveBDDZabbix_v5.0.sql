06138889f2f824d22ffefebfd2e87be1d674d2fb
root <root@atower.labrhel8>
2020-12-20 07:10:10 -0800

    Agregar archivo sql para remover los elementos de la base de datos

---
+++ RemoveBDDZabbix_v5.0.sql
@@ -0,0 +1,14 @@
+-- Remover los elementos de la base de datos de Zabbix
+
+-- Eliminar la base de datos
+DROP DATABASE ZABBIX;
+
+-- Eliminar el usuario de Zabbix
+DROP USER 'zabbix'@'localhost';
+
+-- Commiteamos los cambios
+COMMI
+
+-- Commiteamos los cambios
+COMMIT;
+
